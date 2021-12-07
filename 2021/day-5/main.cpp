4d4ba79eef3400570420be64c6aedc134fac3f90
Justin Hoang <justinhoang@mines.edu>
2021-12-06 17:06:39 -0700

    created day 5 files

---
+++ 2021/day-5/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    // read in the file
+    ifstream fileIn("sample.txt");
+    if (!fileIn) {
+        return -1;
+    }
+
+
+}
