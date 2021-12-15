a81b8f73919ea7b7e3307bd72e1620d117b96532
Justin Hoang <justinhoang@mines.edu>
2021-12-15 13:52:08 -0700

    testing on clion

---
+++ 2021/day-5-clion/Point.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Justin on 12/15/2021.
+//
+
+#include "Point.h"
---
+++ 2021/day-5-clion/Point.h
@@ -0,0 +1,14 @@
+//
+// Created by Justin on 12/15/2021.
+//
+
+#ifndef DAY_5_CLION_POINT_H
+#define DAY_5_CLION_POINT_H
+
+
+class Point {
+
+};
+
+
+#endif //DAY_5_CLION_POINT_H
