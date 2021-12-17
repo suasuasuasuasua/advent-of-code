f24cc0dad71f2095fdf7ec0160c9d19e64b75bf4
Justin Hoang <72476123+suasuasuasuasua@users.noreply.github.com>
2021-12-16 17:00:30 -0700

    finished day 5

--- 2021/day-5/cmake-build-debug/CMakeFiles/3.22.1/CMakeCCompiler.cmake
+++ 2021/day-5/cmake-build-debug/CMakeFiles/3.22.1/CMakeCCompiler.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_C_COMPILER "/usr/bin/x86_64-linux-gnu-gcc-10")
+set(CMAKE_C_COMPILER "/bin/x86_64-linux-gnu-gcc-10")
 set(CMAKE_C_COMPILER_ARG1 "")
 set(CMAKE_C_COMPILER_ID "GNU")
 set(CMAKE_C_COMPILER_VERSION "10.2.1")
@@ -21,11 +21,11 @@ set(CMAKE_C_SIMULATE_VERSION "")
 
 
 
-set(CMAKE_AR "/usr/bin/x86_64-linux-gnu-ar")
-set(CMAKE_C_COMPILER_AR "/usr/bin/x86_64-linux-gnu-gcc-ar-10")
-set(CMAKE_RANLIB "/usr/bin/x86_64-linux-gnu-ranlib")
-set(CMAKE_C_COMPILER_RANLIB "/usr/bin/x86_64-linux-gnu-gcc-ranlib-10")
-set(CMAKE_LINKER "/usr/bin/x86_64-linux-gnu-ld")
+set(CMAKE_AR "/bin/x86_64-linux-gnu-ar")
+set(CMAKE_C_COMPILER_AR "/bin/x86_64-linux-gnu-gcc-ar-10")
+set(CMAKE_RANLIB "/bin/x86_64-linux-gnu-ranlib")
+set(CMAKE_C_COMPILER_RANLIB "/bin/x86_64-linux-gnu-gcc-ranlib-10")
+set(CMAKE_LINKER "/bin/x86_64-linux-gnu-ld")
 set(CMAKE_MT "")
 set(CMAKE_COMPILER_IS_GNUCC 1)
 set(CMAKE_C_COMPILER_LOADED 1)
