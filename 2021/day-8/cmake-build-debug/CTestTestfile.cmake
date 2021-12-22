88ea1b06db2fc8885f1e1fe21fdd44e98a5f1cbd
Justin Hoang <72476123+suasuasuasuasua@users.noreply.github.com>
2021-12-22 13:41:15 -0700

    finished part 1 of day 9

--- 2021/day-8/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/justinhoang/Documents/advent-code/2021/day-8
-# Build directory: /Users/justinhoang/Documents/advent-code/2021/day-8/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
