8718de8ec597e0bdfdf79a1d6a0d405b87b057d1
Justin Hoang <72476123+suasuasuasuasua@users.noreply.github.com>
2021-12-23 14:52:12 -0700

    started template day-10 probably going to use stacks to finish part 1

--- 2021/day-10/main.cpp
+++ 2021/day-10/main.cpp
@@ -1,8 +1,53 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <stack>
 
 using namespace std;
+
+void solve();
+bool openFile(ifstream &fileIn, const string &fileName);
+vector<string> readFile(ifstream &fileIn);
 
 int main()
 {
     /* start here */
+    solve();
+}
+
+void solve()
+{
+    ifstream fileIn;
+    string fileName = "../sample.txt";
+
+    if (!openFile(fileIn, fileName))
+    {
+        cout << "File could not be opened for reading" << endl;
+        return;
+    }
+
+    vector<string> fileData = readFile(fileIn);
+}
+
+bool openFile(ifstream &fileIn, const string &fileName)
+{
+    fileIn.open(fileName);
+    if (!fileIn)
+    {
+        return false;
+    }
+    return true;
+}
+
+vector<string> readFile(ifstream &fileIn)
+{
+    vector<string> fileData;
+    while (!fileIn.eof())
+    {
+        string line;
+        getline(fileIn, line);
+        fileData.push_back(line);
+    }
+    return fileData;
 }
