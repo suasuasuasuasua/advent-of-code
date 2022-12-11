66ebc14da02d72cdffc15c07f47c7f087b61b8b8
Justin Hoang <72476123+suasuasuasuasua@users.noreply.github.com>
2022-12-11 15:03:40 -0700

    Had help on Day 11 2022 The LCM solution was very clever...

--- 2022/11.py
+++ 2022/11.py
@@ -1,6 +1,6 @@
 import operator
-import numpy as np
 import math
+import functools
 
 input = '''Monkey 0:
   Starting items: 98, 70, 75, 80, 84, 89, 55, 98
@@ -58,41 +58,13 @@ Monkey 7:
     If true: throw to monkey 3
     If false: throw to monkey 6'''
 
-input = '''Monkey 0:
-  Starting items: 79, 98
-  Operation: new = old * 19
-  Test: divisible by 23
-    If true: throw to monkey 2
-    If false: throw to monkey 3
-
-Monkey 1:
-  Starting items: 54, 65, 75, 74
-  Operation: new = old + 6
-  Test: divisible by 19
-    If true: throw to monkey 2
-    If false: throw to monkey 0
-
-Monkey 2:
-  Starting items: 79, 60, 97
-  Operation: new = old * old
-  Test: divisible by 13
-    If true: throw to monkey 1
-    If false: throw to monkey 3
-
-Monkey 3:
-  Starting items: 74
-  Operation: new = old + 3
-  Test: divisible by 17
-    If true: throw to monkey 0
-    If false: throw to monkey 1'''
-
 lines = [[s.strip() for s in l.split('\n')] for l in input.split('\n\n')]
 
 all_monkeys = {}
-
+LCM = 1
 for l in lines:
     monkey = int(l[0].split()[1].strip(':'))
-    items = np.array(list(map(int, l[1].split(': ')[1].split(', '))))
+    items = list(map(int, l[1].split(': ')[1].split(', ')))
     operation = l[2].split(': ')[1].split()[3:]
     test = int(l[3].split(': ')[1].split()[-1])
     throws = [int(i.split()[-1]) for i in l[4:]]
@@ -100,46 +72,41 @@ for l in lines:
     all_monkeys[monkey] = [
         items, operation, test, throws
     ]
+    LCM *= test
 
 operator_dict = {
-    '+': np.add,
-    '*': np.multiply,
+    '+': operator.add,
+    '*': operator.mul,
 }
 
-monkey_inspections = {m: 0 for m in all_monkeys}
-# Run the loop for twenty iterations
-# for i in range(10000):
-for i in range(20):
-    # for i in range(1):
-    # For each monkey in the round
-    print("Round ", i)
-    for monkey, attributes in all_monkeys.items():
-        items, operation, test, throws = attributes
-        # For each item in the monkey's hands
-        for item in items:
-            operand = int(operation[1]) if operation[1].isnumeric() else item
-            # worry = operator_dict[operation[0]](item, operand)
-            if operation[0] == '+':
-                worry = item + operand
-            else:
-                worry = item * operand
-            if worry % test == 0:
-                all_monkeys[throws[0]][0] = np.append(
-                    all_monkeys[throws[0]][0], worry)
-            else:
-                all_monkeys[throws[1]][0] = np.append(
-                    all_monkeys[throws[1]][0], worry)
-            monkey_inspections[monkey] += 1
+def run_rounds(n, p1):
+    monkey_inspections = {m: 0 for m in all_monkeys}
+    for i in range(n):
+        # For each monkey in the round
+        for monkey, attributes in all_monkeys.items():
+            items, operation, test, throws = attributes
+            # For each item in the monkey's hands
+            for item in items:
+                operand = int(operation[1]) if operation[1].isnumeric() \
+                    else item
+                item %= LCM
+                worry = operator_dict[operation[0]](item, operand)
+                worry = math.floor(worry / 3.0) if p1 \
+                    else worry
+                all_monkeys[throws[0]][0].append(worry) if worry % test == 0 \
+                    else all_monkeys[throws[1]][0].append(worry)
 
-        all_monkeys[monkey][0] = np.array([])
+                monkey_inspections[monkey] += 1
+            all_monkeys[monkey][0].clear()
 
-print(monkey_inspections)
-max_inspections = list(sorted(monkey_inspections.items(),
-                       key=lambda x: x[1], reverse=True))
-print(max_inspections[1][1] * max_inspections[0][1])
+    return monkey_inspections
 
-part_1 = 0
+
+part_1 = sorted(run_rounds(20, True).values(), reverse=True)
+part_1 = part_1[0] * part_1[1]
 print(f"Part 1 {part_1}")
 
-part_2 = 0
+part_2 = run_rounds(10000, False)
+part_2 = sorted(part_2.values(), reverse=True)
+part_2 = part_2[0] * part_2[1]
 print(f"Part 2 {part_2}")
